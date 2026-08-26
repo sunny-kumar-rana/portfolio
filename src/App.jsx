c5c87a96bfd8045bb15cb96043d37b6eaf576716
Sunny <sunnysinghhrana@gmail.com>
2026-08-26 17:15:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <h1 className="p-10 text-4xl font-bold">
+        Portfolio
+      </h1>
+    </main>
+  )
+}
+
+export default App
